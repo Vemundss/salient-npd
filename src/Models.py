9dca20ac39e542b30a9786d0d2e7bd78c1922776
Vemund <vemund@live.com>
2021-02-04 16:55:38 +0100

    First commit

---
+++ src/Models.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Dense, Flatten, Input, Softmax
+
+class MindReader(tf.keras.Model):
+	def __init__(self, out_dim, **kwargs):
+		super(MindReader, self).__init__(**kwargs)
+		self.d1 = Dense(units=out_dim)
+		self.sm = Softmax()
+
+	def call(self, inputs: tf.Tensor) -> tf.Tensor:
+		"""
+		NN forward pass
+		Args:
+			inputs: assume shape [mb_size, features]
+
+		Returns: shape [mb_size, out_dim]
+		"""
+		z = self.d1(inputs)
+		return self.sm(z)
---
+++ src/learning-scripts/first.py
@@ -0,0 +1,10 @@
+import os
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from allensdk.brain_observatory.ecephys.ecephys_project_cache import EcephysProjectCache
+
+
+print("ok")
